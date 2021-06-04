556a7acb0bd0e283830a8d6b0afb0fb2ecb06126
Elton Fonseca <elton869@gmail.com>
2021-06-04 12:48:08 -0300

    05 - Variáveis e constantes

---
+++ variaveis-constantes.php
@@ -0,0 +1,23 @@
+<?php
+
+//variáveis
+$nome = 'Elton Fonseca';
+
+echo $nome;
+
+$nome = 100;
+
+echo '<br>';
+echo $nome;
+
+//constantes
+define('ESCOLA', 'Treinaweb');
+
+echo '<br>';
+echo ESCOLA;
+
+const CURSO = 'Trilha PHP';
+
+echo '<br>';
+echo CURSO;
+
