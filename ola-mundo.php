6dbd6b1ff5ff267c072da642e4186d3e40e3916d
Elton Fonseca <elton869@gmail.com>
2021-06-04 12:47:40 -0300

    04 - Porque usamos tags no PHP

--- ola-mundo.php
+++ ola-mundo.php
@@ -1,3 +1,14 @@
-<?php
-
-echo 'Olá Mundo';
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Lista de Diaristas</h1>
+        <p><?php echo 'maria, 23anos, são paulo' ?></p>
+        <p><?= 'Lucia, 55anos, são bernardo' ?></p>
+    </body>
+</html>
