8d1680d563d64389b4f3a2c1c59ea8431f8519fb
Elton Fonseca <elton869@gmail.com>
2021-06-04 13:12:26 -0300

    06 - Operadores Matemáticos

---
+++ operadores.php
@@ -0,0 +1,19 @@
+<?php
+
+$numero1 = 6;
+$numero2 = 10;
+
+echo $numero1 + $numero1;
+echo '<br>';
+echo $numero2 - $numero1;
+echo '<br>';
+echo $numero2 * $numero1;
+
+echo '<br>';
+echo $numero2 / 6;
+
+echo '<br>';
+echo 3 % 2;
+
+echo '<br>';
+echo 2 ** 4;
