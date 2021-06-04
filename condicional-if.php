dbe0bac370faa9c623068335b97d79fecd91713a
Elton Fonseca <elton869@gmail.com>
2021-06-04 16:52:07 -0300

    09 - Condicional If

---
+++ condicional-if.php
@@ -0,0 +1,17 @@
+<?php
+
+$idade = 0;
+
+// if ($idade > 18) {
+//     echo "Maior de 18 anos";
+// } elseif ($idade < 10) {
+//     echo "você tem menos de 10 anos";
+// } else {
+//     echo "Você está entre 10 e 18 anos";
+// }
+
+if ($idade) {
+    echo "caiu no if";
+} else {
+    echo "caiu no else";
+}
