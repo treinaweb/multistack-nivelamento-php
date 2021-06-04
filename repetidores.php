5e59457aaa6c3d77b1d2c5e2d67c9ee4d2ce695e
Elton Fonseca <elton869@gmail.com>
2021-06-04 18:54:34 -0300

    10 - Estruturas de Repetição

---
+++ repetidores.php
@@ -0,0 +1,13 @@
+<?php
+
+// $contador = 1;
+
+// while ($contador <= 10) {
+//     echo "$contador Treinaweb <br>";
+//     $contador++;
+// }
+
+for ($contador=1; $contador <= 10; $contador++) {
+    echo "$contador Treinaweb <br>";
+}
+
