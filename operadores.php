d2db51ebad4f166e72013b869a2f2f839c4c3de3
Elton Fonseca <elton869@gmail.com>
2021-06-04 13:57:15 -0300

    07 - Operadores de comparação

--- operadores.php
+++ operadores.php
@@ -1,19 +1,35 @@
 <?php
 
-$numero1 = 6;
-$numero2 = 10;
 
-echo $numero1 + $numero1;
-echo '<br>';
-echo $numero2 - $numero1;
-echo '<br>';
-echo $numero2 * $numero1;
+// Operadores Matemáticos
+// echo $numero1 + $numero1;
+// echo '<br>';
+// echo $numero2 - $numero1;
+// echo '<br>';
+// echo $numero2 * $numero1;
 
-echo '<br>';
-echo $numero2 / 6;
+// echo '<br>';
+// echo $numero2 / 6;
 
-echo '<br>';
-echo 3 % 2;
+// echo '<br>';
+// echo 3 % 2;
 
-echo '<br>';
-echo 2 ** 4;
+// echo '<br>';
+// echo 2 ** 4;
+
+$numero1 = 10;
+$numero2 = "10";
+
+var_dump($numero1 < $numero2);
+var_dump($numero1 <= $numero2);
+
+var_dump($numero1 > $numero2);
+var_dump($numero1 >= $numero2);
+
+var_dump($numero1 == $numero2);
+var_dump($numero1 === $numero2);
+
+var_dump($numero1 != $numero2);
+var_dump($numero1 !== $numero2);
+
+
