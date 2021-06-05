4fb6087278bf9d417a183f1607dd96e209359c00
Elton Fonseca <elton869@gmail.com>
2021-06-05 13:27:02 -0300

    14 - Funções do PHP

--- funcoes.php
+++ funcoes.php
@@ -1,15 +1,23 @@
 <?php
 
-function somar($numero1, $numero2) {
-    echo 'antes do return ';
+// function somar($numero1, $numero2) {
+//     echo 'antes do return ';
 
-    return $numero1 + $numero2;
+//     return $numero1 + $numero2;
 
-    echo 'Depois do return';
-}
+//     echo 'Depois do return';
+// }
 
-$soma1 = somar(10, 20);
-echo $soma1;
+// $soma1 = somar(10, 20);
+// echo $soma1;
+
+// echo '<br>';
+// echo somar(100, 200);
+
+
+$tamanhoString = strlen('Treinaweb');
+echo $tamanhoString;
 
 echo '<br>';
-echo somar(100, 200);
+$nomeMaiusculo = strtoupper('treinaweb');
+echo $nomeMaiusculo;
